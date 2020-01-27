b88aa6a678fb4e7d3934bf6925e0af0ecd667138
dodgez <zacharysdodge@gmail.com>
2020-01-27 08:46:32 -0800

    Add interrupts

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,14 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     println!("It did not crash!");
-    loop {}
+    rustdev::hlt_loop();
 }
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    rustdev::hlt_loop();
 }
 
 #[cfg(test)]
